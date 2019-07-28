ab77f26d028184abdb38092b921f8d745af6d58a
Mevericks <reloved@gmail.com>
2019-07-28 16:11:02 +0300

    методы регистрации маршрутов/routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,78 @@
 |
 */
 
+Route::get('/', ['as'=>'home',function () {
+    return view('welcome');
+}]);
+
+Route::get('/article/{id}', ['as'=>'article',function ($id) {
+   echo $id;
+}]);
+
+Route::get('/page/{cat}/{id}',function($var1) {
+	
+	echo '<pre>';
+	
+	echo $var1;
+	//print_r($_ENV);
+	//echo config('app.locale');
+	//echo Config::set('app.locale','ru');
+	//echo Config::get('app.locale');
+	///echo env('APP_ENV');
+	
+	echo '</pre>';
+	
+})/*->where(['id'=>'[0-9]+','cat'=>'[A-Za-z]+'])*/;
+
+/*Route::post('/comments',function () {
+	
+	print_r($_POST);
+	
+});*/
+
+/*
+Route::match(['get','post'],'/comments',function () {
+	
+	print_r($_POST);
+	
+});*/
+
+/*Route::any('/comments',function () {
+	
+	print_r($_POST);
+	
+});*/
+
+Route::post('/comments',function () {
+	
+	print_r($_POST);
+	
+});
+
+
+
+Route::group(['prefix'=>'admin/{id}'],function() {
+	
+	
+	Route::get('page/create/{var}',function($id) {
+		
+		$route = Route::current();
+		
+		//echo $route->getName();
+		//echo $route->getParameter('var',24);
+		print_r($route->parameters());
+		
+		//return redirect()->route('article',array('id'=>25));
+	})->name('createpage');
+	
+	Route::get('page/edit',function() {
+		echo 'page/edit';
+	});
+	
+
+});
+
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
