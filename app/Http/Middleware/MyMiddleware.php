0eb9f20f40c2fa71c032eb7b39ff4ed7ecc22b19
Mevericks <reloved@gmail.com>
2019-07-28 17:53:40 +0300

    создание и подключение MyMiddleware (посредники)

---
+++ app/Http/Middleware/MyMiddleware.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class MyMiddleware {
+  /**
+   * Handle an incoming request.
+   *
+   * @param  \Illuminate\Http\Request $request
+   * @param  \Closure $next
+   * @return mixed
+   */
+  public function handle($request, Closure $next, $mparam) {
+    if ($request->route('param') != 'pages' && $mparam == 'test') {
+      return redirect()->route('home');
+    }
+    return $next($request);
+  }
+}
